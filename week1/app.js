eede89b8b35ef47dbf2b371484ff5feeece366a1
chandu <chandrasr.palle@gmail.com>
2018-06-15 10:40:47 +0200

    file reading added with promises

--- week1/app.js
+++ week1/app.js
@@ -1,7 +1,15 @@
 let fs = require('fs');
 let util = require('util');
 
-fs.readFile("text1.txt", 'utf8', (err, data)=>{
-    if(err) throw err;
-console.log(data);
+const readFileP = util.promisify(fs.readFile);
+
+const readTexOne = readFileP("text1.txt", "utf8");
+const readTexTwo = readFileP("text2.txt", "utf8");
+const readTexthree = readFileP("text3.txt", "utf8");
+const readTexFour = readFileP("text4.txt", "utf8");
+
+Promise.all([readTexOne, readTexTwo, readTexthree, readTexFour]).then((data)=>{
+    console.log(data.join(" "));
+}).catch(()=>{
+    console.log("error");
 });
