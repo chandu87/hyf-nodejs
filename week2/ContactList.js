18d9c330c996529cdea774aece8c8878d0ed4c8e
chandu <chandrasr.palle@gmail.com>
2018-06-23 16:40:16 +0200

    data loaded as instace of contact class

--- week2/ContactList.js
+++ week2/ContactList.js
@@ -3,24 +3,41 @@ const fs = require("fs");
 const util = require("util");
 
 const writeFileP = util.promisify(fs.writeFile);
+const readFileP = util.promisify(fs.readFile);
 
 class ContactList{
     constructor(fileName){
         this.list = [];
+        this.loadedData = [];
         this.fileName = fileName;
     }
     add(contact){
-        if(contact instanceof Contact)
+        if(contact instanceof Contact)  
             this.list.push(contact);
         else
         console.log("contact can't be added");
     }
     save(){
-        console.log(this.fileName, this.list);
+
         writeFileP(this.fileName, JSON.stringify(this.list))
-        .then(()=>{console.log("File saved")})
+        .then(()=>{
+            console.log("------ File saved ---------");
+            console.log(this.list);
+        })
         .catch((err)=>{console.log("error saving file" + err)});
 
+    }
+    load(){
+        readFileP(this.fileName)
+        .then((data)=>{
+            // console.log(data);
+           JSON.parse(data).forEach(contact => {
+               this.loadedData.push(new Contact(contact.name, contact.age, contact.phoneNumber));
+           });;
+           console.log("------ File Loaded ------- ");
+        //    console.log(this.loadedData);
+        })
+        .catch((err)=>{console.log("Error Loading File" + err)});
     }
 }
 
--- week2/read.js
+++ week2/read.js
@@ -1,1 +1,17 @@
 console.log("read.js");
+
+const Contact = require("./Contact.js");
+const ContactList = require("./ContactList");
+
+let person1 = new Contact("Richard", "30", "");
+let person2 = new Contact("Mat", "30", "74837383");
+
+const list = new ContactList("hyf-contacts.json");
+list.add(person1);
+list.add(person2);
+list.save();
+list.load();
+setTimeout(function() {
+  console.log(list.loadedData);
+  console.log("Loaded data is instace of Contact : ",list.loadedData[0] instanceof Contact);
+}, 1000);
