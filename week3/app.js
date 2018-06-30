b7ef0a5e8742c5153aa0fd8c3d3e1429fe37ed49
chandu <chandrasr.palle@gmail.com>
2018-06-30 14:16:35 +0200

    code alignment changed

--- week3/app.js
+++ week3/app.js
@@ -1,27 +1,35 @@
-console.log("Hello");
 
-const http = require('http');
+const http = require("http");
 let state = 10;
-http.createServer(function(req, res){
+
+http
+  .createServer(function(req, res) {
     // console.log(req);
     // console.log(req.url);
-    if(req.url=="/"){
-        res.write("Helooo");
-        res.end();    
-    }else if(req.url == "/state"){
-        res.write(`${state}`);
-        res.end();    
-    }else if(req.url == "/add"){
-        res.write(`${state=state+1}`);
-        res.end();    
-    }else if(req.url == "/remove"){
-        res.write(`${state=state-1}`);
-        res.end();    
-    }else if(req.url == "/reset"){
-        res.write(`${state=10}`);
-        res.end();    
-    }else if(req.url == "/substract"){
-        res.write("Error 404 : Not Found");
-        res.end();    
+    if (req.url == "/") {
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.write("Index Route");
+      res.end();
+    } else if (req.url == "/state") {
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.write(`${state}`);
+      res.end();
+    } else if (req.url == "/add") {
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.write(`${(state = state + 1)}`);
+      res.end();
+    } else if (req.url == "/remove") {
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.write(`${(state = state - 1)}`);
+      res.end();
+    } else if (req.url == "/reset") {
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.write(`${(state = 10)}`);
+      res.end();
+    } else if (req.url == "/substract") {
+      res.writeHead(404, { "Content-Type": "text/html" });
+      res.write("Error 404 : Not Found");
+      res.end();
     }
-}).listen(3000);
+  })
+  .listen(3000);
